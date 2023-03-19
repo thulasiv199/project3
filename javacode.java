9d7be47e80ade28318ff05fb3436bd28d2d822fe
thulasi <thulasiv199@gmail.com>
2023-03-19 16:18:10 -0400

    commit2

--- javacode.java
+++ javacode.java
@@ -2,6 +2,7 @@ public class Javacode {
   public static void main(String[] args) {
     System.out.println("Hello World");
     System.out.println("Hello commit-1");
+    System.out.println("Hello commit-2");
 
   }
 }
